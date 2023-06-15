0d8e1035625a2d23c73161a84198e309edec3fcc
Lepyoha <94984429+Lepyoha@users.noreply.github.com>
2023-06-15 16:16:30 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ new Swiper('.banner .swiper', {
     direction: 'horizontal',
     loop: true,
     speed: 800,
+    autoHeight: true,
 
     autoplay: {
         delay: 15000,
@@ -29,10 +30,23 @@ new Swiper('.tariffs .swiper', {
   slidesPerView: 3,
   watchOverflow: true,
 
+
   navigation: {
     nextEl: '.tariffs .swiper .swiper-button-next',
     prevEl: '.tariffs .swiper .swiper-button-prev',
   },
+
+  breakpoints:{
+    360:{
+      slidesPerView: 1,
+    },
+    760:{
+      slidesPerView: 2,
+    },
+    1380:{
+      slidesPerView: 3,
+    }
+  }
 
 });
 
